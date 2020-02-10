abc081bb423168e29b4cc60374a848f58918ae26
michalszmigiel <michal.r.szmigiel@gmail.com>
2020-02-10 14:07:19 +0100

    added composer

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,10 +4,5 @@ $app = [];
 
 $app['config'] = require 'config.php';
 
-require 'core/Router.php';
-require 'core/Request.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
-
 // $pdo = Connection::make();
 $app['database'] = new QueryBuilder(Connection::make($app['config']['database']));
